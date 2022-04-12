122fbda2bfe0b19c6a5c6ab0e5d4f4e007c1235b
Dvlpius <101324558+Dvlpius@users.noreply.github.com>
2022-04-12 10:41:54 -0400

    SLQ::Qry
    Add  2 Exmaple;

--- SQL::Qry/Qry.c
+++ SQL::Qry/Qry.c
@@ -2,29 +2,23 @@
 ///Des=This builtin function directly Exec query and
 ///    send handle to inner script , no need external scripts
 ///QryHdl=return data recieved from query directl like event handle and
-///       no need variable.
+///       no need external variable similar this.
+///QryHdl==this;
 /////////////////////////////////////////////////////////////////////
 
 
 
 ///[Example_0]/////////////////////////////
-public void Example_0()
-{
-  Qry("SELECT * FROM _History WHERE Date='Sunday'"){
+public int Qry("SELECT * FROM _History WHERE Date='Sunday'"){
     return QryHdl.Data.ToInteger();
-  }
 }
 
 
 
 ///[Example_1]/////////////////////////////
-public void Example_1()
-{
-  
-  Qry("SELECT * FROM _History WHERE Date='Sunday'"){
+public void Qry("SELECT * FROM _History WHERE Date='Sunday'"){
     Qry("SELECT * FROM ${QryHdl.Data.ToInteger()} WHERE >10"){
       Http.Snd(QryHdl.Data);
     }
-  }    
-  
-}
+}    
+
